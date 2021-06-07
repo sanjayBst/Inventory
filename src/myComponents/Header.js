2d91b422b00cf0ef9708cb6e1046bbc17c7c0223
sanjayBst <sanjaybisht442@gmail.com>
2021-06-07 09:08:28 +0530

    error removed

--- src/myComponents/Header.js
+++ src/myComponents/Header.js
@@ -5,11 +5,12 @@ export default function Header() {
   return (
     <div>
      <nav className="k"> 
-     <a className="active">Home</a>
-     <a>Products</a>
-     <a>Contact Us</a>
-     <a>About</a>
+     <a className="active " href="www.google.com">Home</a>
+     <a href="www.google.com"  >Products</a>
+     <a href="www.google.com" >Contact Us</a>
+     <a href="www.google.com" >About</a>
       </nav>
     </div>
   )
-}
+
+  }
