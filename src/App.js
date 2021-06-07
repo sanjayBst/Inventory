e4b0571531b2fe1fc9a4d0ccb1dd0832964089c5
sanjayBst <sanjaybisht442@gmail.com>
2021-06-07 08:55:11 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import Header from './myComponents/Header';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/myComponents/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+  return (
+    <div>
+     <nav className="k"> 
+     <a className="active">Home</a>
+     <a>Products</a>
+     <a>Contact Us</a>
+     <a>About</a>
+      </nav>
+    </div>
+  )
+}
